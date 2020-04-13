158b3070a7c8fa11c54ab25444089f85440a7a07
a1824jj <a1824jj@aiit.ac.jp>
2020-04-13 10:37:44 +0900

    make ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todolistsampel/screens/tasks_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -6,6 +8,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
