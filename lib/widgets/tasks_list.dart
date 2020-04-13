07498f7d41179a2c2d970c056281cbee170e38d5
a1824jj <a1824jj@aiit.ac.jp>
2020-04-13 13:06:43 +0900

    checking onoff - use provider

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,30 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todolistsampel/models/task.dart';
 import 'package:todolistsampel/models/task_data.dart';
 import 'package:todolistsampel/widgets/task_tile.dart';
 
-class TasksList extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _TaskList();
-  }
-}
+class TasksList extends StatelessWidget {
 
-class _TaskList extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
       builder: (context, taskData, child){
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
             return TaskTile(
-              taskTitle: taskData.tasks[index].name,
-              isChecked: taskData.tasks[index].isDone,
+              taskTitle: task.name,
+              isChecked: task.isDone,
               checkboxCallback: (newValue) {
-//            setState(() {
-//              Provider.of<TaskData>(context).tasks[index].toggleDone();
-//            });
+                taskData.updateTask(task);
               },
             );
           },
