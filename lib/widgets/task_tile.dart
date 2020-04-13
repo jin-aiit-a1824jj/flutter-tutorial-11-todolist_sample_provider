400800e81ecee76fc35889b2c8c4c4bda909d6ec
a1824jj <a1824jj@aiit.ac.jp>
2020-04-13 11:27:26 +0900

    use delegate & global field,  chaging checkbox statelesswieget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State createState() {
+    return _TaskTileState();
+  }
+}
+
+class _TaskTileState extends State<TaskTile> {
+  @override
+  Widget build(BuildContext context) {
+
+    return ListTile(
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckBox(
+          checkboxStatue: isChecked,
+          toggleCheckboxState: (checkboxState){
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }),
+    );
+  }
+
+  bool isChecked = false;
+
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxStatue;
+  final Function toggleCheckboxState;
+
+  TaskCheckBox({this.checkboxStatue, this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-      ),
+    return Checkbox(
+      value: checkboxStatue,
+      onChanged: toggleCheckboxState,
     );
   }
 }
